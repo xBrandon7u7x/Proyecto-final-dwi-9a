95c3329f253b4d4f576a93be9f4faee566c197f5
Brandon Flores <brandonflores.ft@gmail.com>
2022-12-03 17:44:58 -0600

    first commit

---
+++ inc/footer.php
@@ -0,0 +1,39 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    Contamos con variedad de productos a excelentes precios y excelente calidad.</br>
+                
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Aguascalientes, Ags.</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true"> 4493436700</p><a href="#" target="_blank" style="color: #5bc0de"> Envianos un mensaje</a></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; baft@gmail.com</a>
+            </div>
+        </div>
+    </div>
+</footer>
